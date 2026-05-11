3b356292a6c409c0e2e339879da643d5001b3258
jhs512 <jangka512@gmail.com>
2026-05-11 14:45:31 +0900

    011

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,12 +1,18 @@
 package com.back;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class AppConfig {
+    @Autowired
+    @Lazy
+    private AppConfig self;
+
     @Bean
     int version() {
         return 55;
